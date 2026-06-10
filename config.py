a89eee7c2906041e332ee39b67d4bfd0ca785dff
polay90 <polayvbt@gmail.com>
2026-06-10 21:32:19 +0700

    Add placeholders for PHONE_NUMBER_ID and WHATSAPP_ACCESS_TOKEN

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID', '@play1990')
 JAMSOSTEK_API_URL = os.getenv('JAMSOSTEK_API_URL', 'https://api.jamsostek.go.id')
+PHONE_NUMBER_ID =
+WHATSAPP_ACCESS_TOKEN =
