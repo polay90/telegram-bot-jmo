c517fb4dc831776ca0312bb42430098372540680
polay90 <polayvbt@gmail.com>
2026-06-10 22:15:48 +0700

    Refactor config.py for Telegram bot settings
    Updated configuration for Telegram bot and removed environment variable usage.

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-import os
-from dotenv import load_dotenv
+# ================== CONFIG JMO BOT (Minimal) ==================
 
-load_dotenv()
+# Telegram
+TELEGRAM_BOT_TOKEN = "8675062051:AAHqAR1GLUi9cRVSDoFWuNrGtOkU6mS71L8"
 
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID', '@play1990')
-JAMSOSTEK_API_URL = os.getenv('JAMSOSTEK_API_URL', 'https://api.jamsostek.go.id')
-WHATSAPP_TOKEN = "JCXAf-vzhaSiyd0SQvfKlzd6AjU"          # Dari Meta
-WHATSAPP_PHONE_ID = "1556975352706684"             # Dari Meta
-WHATSAPP_ADMIN_NUMBER = "62895423349883"                # Nomor admin WhatsApp
+# Admin Telegram (Chat ID)
+ADMIN_CHAT_ID = 1823591491   # Ganti dengan Chat ID admin kamu
+                             # Cara dapat: kirim /id ke @userinfobot
+
+# Optional
+ENV = "production"
