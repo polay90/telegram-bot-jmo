b4361eaf4ab277c319dae1369e0ab7fae6870107
polay90 <polayvbt@gmail.com>
2026-06-10 21:58:11 +0700

    Load ADMIN_CHAT_ID from environment variable

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-ADMIN_CHAT_ID =
+
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID', '@play1990')
 JAMSOSTEK_API_URL = os.getenv('JAMSOSTEK_API_URL', 'https://api.jamsostek.go.id')
