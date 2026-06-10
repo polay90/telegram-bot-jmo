eb81f673763c9183e868b430315e243d0d9b8615
polay90 <polayvbt@gmail.com>
2026-06-10 22:05:12 +0700

    Update WhatsApp token and phone ID in config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID', '@play1990')
 JAMSOSTEK_API_URL = os.getenv('JAMSOSTEK_API_URL', 'https://api.jamsostek.go.id')
-WHATSAPP_TOKEN = "YOUR_WHATSAPP_ACCESS_TOKEN"          # Dari Meta
-WHATSAPP_PHONE_ID = "YOUR_PHONE_NUMBER_ID"             # Dari Meta
+WHATSAPP_TOKEN = "JCXAf-vzhaSiyd0SQvfKlzd6AjU"          # Dari Meta
+WHATSAPP_PHONE_ID = "1556975352706684"             # Dari Meta
 WHATSAPP_ADMIN_NUMBER = "62895423349883"                # Nomor admin WhatsApp
