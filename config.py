53c5dbf468ec7fb36982e1bbab10022a7a3f5c10
polay90 <polayvbt@gmail.com>
2026-06-10 20:12:07 +0700

    Add configuration for environment variables

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID', '@play1990')
+JAMSOSTEK_API_URL = os.getenv('JAMSOSTEK_API_URL', 'https://api.jamsostek.go.id')
